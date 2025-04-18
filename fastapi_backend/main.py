892d728714971f1d40de506580ba0c7b3f404ccc
Ahmet Çavuşoğlu <acavusoglu@ipekbilgisayar.com>
2025-04-19 01:24:39 +0300

    Çoğu sayfa arayüzü tamamlandı

--- fastapi_backend/main.py
+++ fastapi_backend/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
+from auth import router as auth_router
 
 app = FastAPI()
-
-@app.get("/")
-def home():
-    return {"message": "FastAPI çalışıyor!"}
+app.include_router(auth_router)
