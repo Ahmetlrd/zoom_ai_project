a2109a0d129d93cf39dbcd7fca3a4d294c32aa4e
Ahmet Çavuşoğlu <acavusoglu@ipekbilgisayar.com>
2025-05-02 17:57:52 +0300

    Wireframe added

---
+++ flutter_app/lib/services/zoom_service.dart
@@ -0,0 +1,26 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'secure_storage_service.dart';
+
+class ZoomService {
+  static Future<Map<String, dynamic>?> fetchUserInfo() async {
+    
+    final token = await readAccessToken();
+
+    if (token == null) return null;
+
+    final response = await http.get(
+      Uri.parse('https://api.zoom.us/v2/users/me'),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+    
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      print("Failed to fetch user info: ${response.body}");
+      return null;
+    }
+    
+  }
+}
